09b7f7e6e756d7fa2abee676324d5e27d08831db
Alejandro Nanez <alejonanez@gmail.com>
2017-03-15 20:54:34 -0500

    Update styles

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,41 @@
 import './scss/style.scss';
 
-import { Observable } from 'rxjs';
+import {
+	Observable,
+	BehaviorSubject
+} from 'rxjs';
 import { partial, flowRight } from 'lodash';
 
-import { getFullMonth } from './lib/date_helpers';
+import {
+	getFullMonth,
+	getCurrentMonth,
+	getCurrentDay,
+	getCurrentYear
+} from './lib/date_helpers';
 import { getCalendarTableHTML } from './lib/dom_helpers';
 
 const calendarElement: HTMLElement | null = document.getElementById('calendar');
 const calendarContainer: HTMLElement | null = document.getElementById('calendar-container');
 const bodyElement: HTMLBodyElement | null = document.querySelector('body');
 const closeButton: HTMLElement | null = document.getElementById('close-overlay');
+
+type InitialState = {
+	rawDate: Date,
+	currentDay: number,
+	currentMonth: number,
+	currentYear: number
+}
+
+const initialDate = new Date();
+
+const initialState: InitialState = {
+	rawDate: initialDate,
+	currentDay: getCurrentDay(initialDate),
+	currentMonth: getCurrentMonth(initialDate),
+	currentYear: getCurrentYear(initialDate)
+};
+
+const appState = new BehaviorSubject(initialState);
 
 if (calendarElement && calendarContainer && bodyElement && closeButton) {
 	Observable
